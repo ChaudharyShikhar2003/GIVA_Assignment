ff3e84c27c1cb60fce1601a87c41aec7eb45f922
ChaudharyShikhar2003 <shikharc22@iitk.ac.in>
2024-10-26 18:19:10 +0530

    deploying dummy branch

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', 
-  basePath: '/GIVA_Assignment',
-  trailingSlash: true,  
+  // output: 'export', 
+  // basePath: '/GIVA_Assignment',
+  // trailingSlash: true,  
 };
 
 export default nextConfig;
